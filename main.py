88d47f02f8481b79c7a78688c619b17d304b18e6
toby <tadams@bmcc.nsw.gov.au>
2020-02-04 16:50:53 +1100

    Fixes for google could

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-
 # -*- coding: utf-8 -*-
-import json
+import urllib, json
 import pandas as pd
+import numpy as np
+from flask import request
+from google.cloud import storage
 
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	json_string = '[{"type":"snake","status":"complete","start_time":1566726780000,"sport":"nfl","settings":{"teams":4,"slots_wr":3,"slots_te":2,"slots_super_flex":1,"slots_rb":3,"slots_qb":2,"slots_flex":2,"slots_def":1,"slots_bn":8,"rounds":24,"player_type":0,"pick_timer":60,"cpu_autopick":1,"alpha_sort":0},"season_type":"regular","season":"2019","metadata":{"scoring_type":"dynasty","name":"Dynasty","description":""},"league_id":"471237557296820224","last_picked":1566728606783,"last_message_time":1566728606859,"last_message_id":"471297572451905536","draft_order":{"466816403391901696":4,"466181495111806976":1,"465431267769315328":3,"436876366495870976":2},"draft_id":"471237560434159616","creators":["466181495111806976"],"created":1566714298871}]'
-	league_string = '[{"user_id":"436876366495870976","settings":null,"metadata":{"user_message_pn":"on","transaction_waiver":"on","transaction_trade":"on","transaction_free_agent":"on","transaction_commissioner":"on","team_name_update":"on","team_name":"Johnson and Johnson","player_nickname_update":"on","mention_pn":"on","mascot_message_leg_2":"","mascot_message_emotion_leg_2":"idle","mascot_message_emotion_leg_1":"idle_nervous","mascot_message":"on","mascot_item_type_id_leg_1":"","allow_pn":"on"},"league_id":"471237557296820224","is_owner":null,"is_bot":false,"display_name":"MarkyJ","avatar":"56954d15232c82270bc35562afa6db5f"},{"user_id":"465431267769315328","settings":null,"metadata":{"team_name":"Never Fournette","mention_pn":"on","mascot_message_leg_5":"Dude.  Do you even lift?","mascot_message_leg_4":"","mascot_message_leg_2":"","mascot_message_leg_14":"Smoked","mascot_message_leg_11":"","mascot_message_leg_1":"","mascot_message_emotion_leg_5":"idle","mascot_message_emotion_leg_4":"idle","mascot_message_emotion_leg_14":"idle","mascot_message_emotion_leg_13":"idle_happy","mascot_message_emotion_leg_11":"idle","mascot_message_emotion_leg_1":"","mascot_item_type_id_leg_17":"","mascot_item_type_id_leg_16":"","mascot_item_type_id_leg_15":"","mascot_item_type_id_leg_14":"","mascot_item_type_id_leg_13":"","allow_pn":"on"},"league_id":"471237557296820224","is_owner":null,"is_bot":false,"display_name":"MSorrenson","avatar":"9cf0ba07b858634478ff0edec78d0412"},{"user_id":"466181495111806976","settings":null,"metadata":{"mention_pn":"on","mascot_message_leg_5":"","mascot_message_leg_3":"","mascot_message_leg_2":"Smoked","mascot_message_leg_14":"","mascot_message_leg_11":"It’s been a long time between drinks - was just wondering if you still knew how to spell it","mascot_message_emotion_leg_3":"idle","mascot_message_emotion_leg_2":"idle","mascot_message_emotion_leg_11":"idle","mascot_item_type_id_leg_9":"","mascot_item_type_id_leg_8":"","mascot_item_type_id_leg_7":"","mascot_item_type_id_leg_6":"","mascot_item_type_id_leg_5":"","mascot_item_type_id_leg_4":"","mascot_item_type_id_leg_3":"","mascot_item_type_id_leg_17":"","mascot_item_type_id_leg_16":"","mascot_item_type_id_leg_15":"","mascot_item_type_id_leg_14":"","mascot_item_type_id_leg_13":"","mascot_item_type_id_leg_12":"","mascot_item_type_id_leg_11":"","mascot_item_type_id_leg_10":"","allow_pn":"on"},"league_id":"471237557296820224","is_owner":true,"is_bot":false,"display_name":"skzm","avatar":null},{"user_id":"466816403391901696","settings":null,"metadata":{"team_name":"Kamara shy","mention_pn":"on","mascot_message_leg_4":"SNAP!","mascot_message_leg_3":"If I cut a loud fart you will short circuit","mascot_message_leg_2":"All season mofo","mascot_message_leg_1":"I must break you","mascot_message_emotion_leg_7":"idle","mascot_message_emotion_leg_5":"idle","mascot_message_emotion_leg_4":"action_hurt01","mascot_message_emotion_leg_3":"idle","mascot_message_emotion_leg_2":"idle","mascot_message_emotion_leg_1":"idle","mascot_item_type_id_leg_9":"","mascot_item_type_id_leg_8":"","mascot_item_type_id_leg_7":"","mascot_item_type_id_leg_6":"","mascot_item_type_id_leg_5":"","mascot_item_type_id_leg_4":"","mascot_item_type_id_leg_3":"","mascot_item_type_id_leg_2":"","mascot_item_type_id_leg_17":"","mascot_item_type_id_leg_16":"","mascot_item_type_id_leg_15":"","mascot_item_type_id_leg_14":"","mascot_item_type_id_leg_13":"","mascot_item_type_id_leg_12":"","mascot_item_type_id_leg_11":"","mascot_item_type_id_leg_10":"","mascot_item_type_id_leg_1":"","allow_pn":"on"},"league_id":"471237557296820224","is_owner":null,"is_bot":false,"display_name":"AKamara41","avatar":"55d84a9842931cd39b70ed61cda09c06"}]'
+@app.route('/<id>')
+def hello_world(id):
+	CLOUD_STORAGE_BUCKET = 'diesel-rhythm-93511.appspot.com'
+	gcs = storage.Client()
+	bucket = gcs.bucket(CLOUD_STORAGE_BUCKET)
+	
+	if not id:
+		id='471237557296820224'
+	
+	with urllib.request.urlopen("https://api.sleeper.app/v1/league/" + id + "/users") as url:
+		league = json.loads(url.read().decode())
 
-	with open("1.txt", "r") as read_file:
-	    data = json.load(read_file)
+	with urllib.request.urlopen("https://api.sleeper.app/v1/league/" + id + "/drafts") as url:
+		drafts = json.loads(url.read().decode())
 
-	with open("2.txt", "r") as read_file:
-	    player = json.load(read_file)
+	with urllib.request.urlopen("https://api.sleeper.app/v1/draft/" + drafts[0]['draft_id'] + "/picks") as url:
+		data  = json.loads(url.read().decode())
 
-	with open("3.txt", "r") as read_file:
-	    stats = json.load(read_file)
-
-	df = pd.DataFrame.from_dict(stats).transpose() 
-	league = json.loads(league_string)
-	#df1 = df.sort_values('pts_ppr', ascending=False).head(1).join(player, on='player_id')
+	player = json.loads(bucket.blob('players.txt').download_as_string())	
+	stats = json.loads(bucket.blob('stats.txt').download_as_string())
 
 	draft_df = pd.DataFrame.from_dict(data)
 	league_df = pd.DataFrame.from_dict(league)
@@ -36,17 +40,58 @@ def hello_world():
 	draft_summary3['position_rank'] = draft_summary3.groupby('position')['pts_ppr'].rank(ascending=False)
 	draft_summary3['position_average'] = draft_summary3.groupby('position')['pts_ppr'].mean()
 	draft_summary3['overall_rank'] = draft_summary3['pts_ppr'].rank(ascending=False)
+	
+	#return draft_summary3.to_html(header="true", table_id="table")
+	return render_template('index.html',  tables=[draft_summary3.to_html(classes='data')], titles=draft_summary3.columns.values)
+	
+@app.route('/players')
+def graph():
+	position = request.args.get('position') if  request.args.get('position') != None else 'RB'
+	stattype = request.args.get('stattype') if  request.args.get('stattype') != None else 'pts_ppr'
+	try:
+		playercount = int(request.args.get('playercount'))
+	except (ValueError, TypeError):
+		playercount = 20
+		
+	CLOUD_STORAGE_BUCKET = 'diesel-rhythm-93511.appspot.com'
+	gcs = storage.Client()
+	bucket = gcs.bucket(CLOUD_STORAGE_BUCKET)
+	
+	dfsingle= pd.DataFrame(data = {'player_id': ['6151', '2315']})	
+	dfsingle.set_index('player_id', inplace=True)
+	
+	
+	dfpl = player = json.loads(bucket.blob('players.txt').download_as_string())
+	stats = json.loads(bucket.blob('stats.txt').download_as_string())
+		
+	stats_df = pd.DataFrame.from_dict(stats).transpose()
+	player_df = pd.DataFrame.from_dict(dfpl).transpose()
+	dfax = pd.merge(stats_df, player_df, how='left', right_on='player_id', left_index=True) #	
+	
+	dfx = dfax.loc[dfax['position'] == position].sort_values(stattype, ascending= False).head(playercount)[[stattype,'position', 'full_name']]
+	dfx.rename(columns={stattype: stattype + '_full'}, inplace = True)	
+	for i in range(1,16):	
+		s1 = json.loads(bucket.blob('s'  + str(i) + '.txt').download_as_string())
+		
+		wstats_df = pd.DataFrame.from_dict(s1).transpose()[[stattype]]
+		dfsingle = pd.merge(dfsingle, wstats_df, how='left',  left_index=True , right_index=True)		
+		dfsingle.rename(columns={stattype: 'week' + str(i)}, inplace = True)
+		dfx = pd.merge(dfx, wstats_df, how='left', left_index=True, right_index=True)
+		dfx.rename(columns={stattype: 'week' + str(i)}, inplace = True)
+		
+	
+	print(dfsingle)
+	st = dfx.describe()
+	del st[stattype + '_full']
+	dfx['showvalues'] =  ['No' for i in range(0, len(dfx.index))]
 
-	#for x in data:
-	#	print x['metadata']['last_name']
-	#	print stats['4881']
-
-	league = json.loads(league_string)
-	#for g in league:
-	#	print(g['display_name'])
-
-	draft = json.loads(json_string)
-	return draft_summary3.to_html(header="true", table_id="table")
+	chart = {"renderTo": 'chart_ID', "type": 'line', "height": 350,}
+	
+	series = [{"name": 'Top' + str(playercount) + '  Avg', "data": st.ix['mean'].replace(np.nan, 'null').tolist()}]
+	title = {"text": 'Week'}
+	xAxis = {"categories": ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10', 'W11', 'W12', 'W13', 'W14', 'W15']}
+	yAxis = {"title": {"text": 'yAxis ' + stattype}}
+	return render_template('players.html',  data = json.dumps(dfx.replace(np.nan, 'null').values.tolist()), titles=json.dumps(dfx.columns.values.tolist()), chartID='chart_ID', chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis, position=position, playercount=playercount)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
